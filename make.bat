1ce889c306c2e8558e32602e6cf62a31dce03b0a
Robbbert <Robbbert@users.noreply.github.com>
2017-09-22 11:21:37 +1000

    *stuff that should be ignored

--- make.bat
+++ make.bat
@@ -1,1 +1,6 @@
+@if exist scripts\minimaws\minimaws.sqlite3 del scripts\minimaws\minimaws.sqlite3
+del mamearcade.sym
+:start
+@if exist mamearcade.exe del mamearcade.exe
+@if exist mamearcade.exe goto start
 make32 -j4 %1 %2 %3
